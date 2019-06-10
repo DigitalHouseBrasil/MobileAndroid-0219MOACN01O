44b8d743f7c89578400d961cd796d3aa7ac0d861
tairoroberto <tairoroberto@hotmail.com>
2019-06-10 17:00:49 -0300

    Adicionando viewpager com adapter personalizado para titulo

--- Android/RevisaoESharedPrefences/app/src/main/java/br/com/digitalhouse/revisaoesharedprefences/home/view/HomeActivity.java
+++ Android/RevisaoESharedPrefences/app/src/main/java/br/com/digitalhouse/revisaoesharedprefences/home/view/HomeActivity.java
@@ -1,25 +1,48 @@
 package br.com.digitalhouse.revisaoesharedprefences.home.view;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.digitalhouse.revisaoesharedprefences.R;
+import br.com.digitalhouse.revisaoesharedprefences.adapters.ViewpagerAdapter;
+import br.com.digitalhouse.revisaoesharedprefences.model.FragmentViewPagerModel;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        replaceFragment(new HomeFragment());
+
+        toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabs);
+        setSupportActionBar(toolbar);
+
+        ViewpagerAdapter adapter = new ViewpagerAdapter(getSupportFragmentManager(), getFragmentsViewPager());
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(getFragmentsViewPager().size());
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
-    public void replaceFragment(Fragment fragment){
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit();
+    private List<FragmentViewPagerModel> getFragmentsViewPager() {
+        List<FragmentViewPagerModel> fragmentViewPagerModels = new ArrayList<>();
+        fragmentViewPagerModels.add(new FragmentViewPagerModel(new HomeFragment(), "Home"));
+        fragmentViewPagerModels.add(new FragmentViewPagerModel(new HomeFragment(), "Cadastro"));
+        fragmentViewPagerModels.add(new FragmentViewPagerModel(new HomeFragment(), "Favoritos"));
+
+        return fragmentViewPagerModels;
     }
 }
