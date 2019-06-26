cf7336117c2190b4dd58e23dd5e00787dd53c3e8
tairoroberto <tairoroberto@hotmail.com>
2019-06-26 19:15:02 -0300

    Add boot receiver

---
+++ Android/AndroidRoomDatabaseApp/app/src/main/java/br/com/digitalhouse/roomdatabaseapp/services/BootReceiver.java
@@ -0,0 +1,19 @@
+package br.com.digitalhouse.roomdatabaseapp.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import br.com.digitalhouse.roomdatabaseapp.ContatosActivity;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
+            Intent startIntent = new Intent(context, ContatosActivity.class);
+            startIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(startIntent);
+        }
+    }
+}
